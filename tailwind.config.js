0e24cbdbf386199de175deae4de3358ff1ead916
Yasir Hamid <ratherseenu16@gmail.com>
2025-03-05 07:21:21 +0530

    updated chnages

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html", 
-    "./src/**/*.{js,jsx,ts,tsx}" // Ensure all React files are included
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},
